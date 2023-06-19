6b2cb00d5e184e8f01a4ffab9cf706107ecb5f61
hemant10singh <singh10hemant99@gmail.com>
2023-06-19 18:14:20 +0530

    login page update

--- amazonprimeclone/lib/main.dart
+++ amazonprimeclone/lib/main.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Amazon Prime Clone',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home:  LoginPage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
-
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -33,7 +39,6 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text("Amazon Prime"),
       ),
       body: Center(
